543fb9e55650d51f6294c3a59cda358ffef13124
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2022-02-07 22:12:37 +0300

    Add files via upload

---
+++ Task13/Lab13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static int Method1(int a)
+    {
+        if (a % 2 == 0)
+            return a / 2;
+        else return 0;
+    }
+    public static void Main()
+    {
+        int a = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(Method1(a));
+    }
+}
